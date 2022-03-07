c56ee8f4ebed75bfe55ffb0886d8c07d3f0511c3
Raí Lima F <rai.lima17@gmail.com>
2022-03-07 19:13:04 -0300

    Initial commit

---
+++ src/main/java/com/ironia/springdeliveryapi/SpringDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.ironia.springdeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeliveryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeliveryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ironia/springdeliveryapi/SpringDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironia.springdeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
