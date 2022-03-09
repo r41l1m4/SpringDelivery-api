3938bfad62e50b33b76b60d9f7ba31ea48fda744
Raí Lima F <rai.lima17@gmail.com>
2022-03-09 17:11:46 -0300

    add jpa annotation to model

--- src/main/java/com/ironia/springdeliveryapi/controller/ClienteController.java
+++ src/main/java/com/ironia/springdeliveryapi/controller/ClienteController.java
@@ -4,27 +4,19 @@ import com.ironia.springdeliveryapi.domain.model.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente c1 = new Cliente();
-        c1.setId(1L);
-        c1.setNome("Raí");
-        c1.setTelefone("98364-78232");
-        c1.setEmail("rai@ironia.com");
-
-        Cliente c2 = new Cliente();
-        c2.setId(2L);
-        c2.setNome("Jana");
-        c2.setTelefone("98364-78232");
-        c2.setEmail("jana@corna.com");
-
-        List<Cliente> lc = List.of(c1, c2);
-
-        return lc;
+        return entityManager.createQuery("from Cliente", Cliente.class)
+                .getResultList();
     }
 }
--- src/main/java/com/ironia/springdeliveryapi/domain/model/Cliente.java
+++ src/main/java/com/ironia/springdeliveryapi/domain/model/Cliente.java
@@ -1,15 +1,25 @@
 package com.ironia.springdeliveryapi.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 
