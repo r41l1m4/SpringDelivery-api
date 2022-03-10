6b1665d8c00d3789939739019744398710de0b08
Raí Lima F <rai.lima17@gmail.com>
2022-03-09 21:07:28 -0300

    add findByNome

--- src/main/java/com/ironia/springdeliveryapi/controller/ClienteController.java
+++ src/main/java/com/ironia/springdeliveryapi/controller/ClienteController.java
@@ -3,19 +3,34 @@ package com.ironia.springdeliveryapi.controller;
 import com.ironia.springdeliveryapi.domain.model.Cliente;
 import com.ironia.springdeliveryapi.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+//        return clienteRepository.findById(clienteId).isPresent() ?
+//                ResponseEntity.ok(clienteRepository.findById(clienteId).get()) :
+//                ResponseEntity.notFound().build();
     }
 }
